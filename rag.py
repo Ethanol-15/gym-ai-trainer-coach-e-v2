fae48bb740d07dbf63929fffb1df940fde47197f
Ethanol-15 <cruz.ethanlyle2003@gmail.com>
2026-05-21 18:29:59 +0800

    Update rag.py

--- rag.py
+++ rag.py
@@ -3,7 +3,7 @@ import faiss
 import numpy as np
 import json
 
-# load your dataset
+# load dataset
 with open('gym_data.json', 'r') as f:
     gym_data = json.load(f)
 
@@ -26,15 +26,30 @@ dimension  = embeddings.shape[1]
 index      = faiss.IndexFlatL2(dimension)
 index.add(embeddings)
 
-def find_relevant_entries(question, top_k=3):
+def find_relevant_entries(question, top_k=3, threshold = 1.2):
+    #encode question to vector
     question_vector = embedder.encode([question])
     question_vector = np.array(
         question_vector
     ).astype('float32')
+
+    #searching for the top matches
     distances, indices = index.search(
         question_vector, top_k
     )
     relevant = []
-    for idx in indices[0]:
-        relevant.append(gym_data[idx])
+    for i, idx in enumerate(indices[0]):
+        distance = distances[0][i]
+
+        # only include the entries below distance threshold
+        # lower distance = more similar 
+        # higher distance = less relevant
+        if distance < threshold:
+            relevant.append(gym_data[idx])
+
+        # if nothing passes the threshold return top 1 as fallback
+        # so that the model LLaMA always has some  context
+        if not relevant:
+            relevant.append(gym_data[indices[0][0]])
+
     return relevant
