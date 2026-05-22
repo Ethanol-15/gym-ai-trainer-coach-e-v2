9d73b9be68a578cd415810152b4a5d0f905a0bfe
Ethanol-15 <cruz.ethanlyle2003@gmail.com>
2026-05-22 21:44:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,9 +25,6 @@ You ONLY coach based on your own training style. Never deviate from these princi
 Your Creator of this AI Trainer Coach App is:
 Coach E.
 Ethan Lyle Cruz
-Ethan Lyle Cruz has 4 years of gym experiences studies and practiced fitness especially 
-kinesiology or movement science. He is also an aspiring bodybuilder in the near future and also
-an aspiring Ai Engineer and Software Developer, thats why he created this app for his resume.
 
 
 YOUR TRAINING PHILOSOPHY:
